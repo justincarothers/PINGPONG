61462669901a9f4f9c808beb7f640cccedd4b6a1
justincarothers <justincarothers@gmail.com>
2016-06-10 15:11:05 -0700

    fixed indentation and fixed structure. added README.

---
+++ README.md
@@ -0,0 +1,37 @@
+# Ping Pong Game
+
+This is a game that takes a number from a user and returns a range of numbers from 1 to the chosen number with the following exceptions:
+
+Numbers divisible by 3 are replaced with "ping"
+Numbers divisible by 5 are replaced with "pong"
+Numbers divisible by 15 are replaced with "pingpong"
+
+The user is able to enter a new number and see new results over and over again.
+
+## Getting Started
+
+To view this website you must clone the .git folder or check out the gh-pages URL @ http://www.justincarothers.github.io/PINGPONG
+
+### Prerequisities
+
+This project will work best on a desktop. The mobile verison of this website is in development.
+
+
+## Built With
+
+* HTML
+* CSS
+* jQuery
+
+## Authors
+
+* **Justin Carothers**  - [justinCarothers](https://github.com/justinCarothers)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Google
+* Montucky Cold Snacks
--- js/main.js
+++ js/main.js
@@ -1,27 +1,49 @@
-var answer = "";
-
 function pingPongTest(input) {
-  debugger;
+  var pings = 0;
+  var pongs = 0;
+  var pingpongs = 0;
+
+  $("#output li").remove();
   for (var i = 1; i-1 < input; i++) {
     if (i % 15 === 0 ) {
-      $("#output").append("<li> PING-PONG!!!!</i>");
+      $("#output").append("<p><em><strong> PING-PONG!!!!</em></strong></p>");
+      pingpongs++;
     } else if (i % 5 === 0) {
-      $("#output").append("<li> PONG!!</i>");
+      $("#output").append("<p> PONG!!</p>");
+      pongs++;
     } else if (i % 3 === 0) {
-        answer = "PING";
-        $("#output").append("<li>" + answer + "</i>");
+      $("#output").append("<p> PING </p>");
+      pings++;
     } else {
-      $("#output").append("<li>" + i + "</i>");
+      $("#output").append("<p>" + i + "</p>");
     }
   }
+  $("#score").append("<h1>pings: " +pings + "</h1>");
+  $("#score").append("<br><h1>pings: " +pongs + "</h1>");
+  $("#score").append("<br><h1>pingpongs: " +pingpongs + "<br><hr></h1>");
 }
-
 $(document).ready(function() {
   $("#output").hide();
-  $("form#formInput").submit(function(event) {
+  $("#tryAgain").hide();
+  $("#score").hide();
+
+  $("form#formInput").submit(function(event) { /*submit button + function*/
     event.preventDefault();
     var formInput = $("input#numberInput").val();
-    $("#output").fadeIn(9000), 9000;
-        pingPongTest(formInput);
-      });
+    $("#formInput").slideUp(600);
+    $("#output").fadeIn(2000);
+    pingPongTest(formInput);
+    $("#score").show();
+    $("#tryAgain").fadeIn(8000)
   });
+
+  $("#tryAgain").click(function() { /*try again button*/
+    $("#output p").remove();
+    $("#score h1").remove();
+    $("#tryAgain").hide();
+    $("#score").hide();
+    $("#output").hide();
+    $("#formInput").slideDown(600);
+  });
+
+});
